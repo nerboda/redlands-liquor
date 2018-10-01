3554feb46515a90344f5c21d9fa4b0a9d86b1a89
Eliathah Boda <nerboda@gmail.com>
2018-09-30 23:02:50 -0600

    replace content and images

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function myMap() {
   var mapCanvas = document.getElementById("map");
-  var myCenter = new google.maps.LatLng(45.579591,9.164906); 
-  var mapOptions = {center: myCenter, zoom: 13};
+  var myCenter = new google.maps.LatLng(39.066958,-108.58777); 
+  var mapOptions = {center: myCenter, zoom: 15};
   var map = new google.maps.Map(mapCanvas,mapOptions);
   var marker = new google.maps.Marker({
     position: myCenter,
